4ae7efc0cb2e29b6a135edd43af106436e4b88e1
rome5200 <144663246+rome5200@users.noreply.github.com>
2024-10-09 18:04:05 +0900

    Add files via upload

---
+++ Chap6_Programming02.java
@@ -0,0 +1,32 @@
+package java001_HelloWorld;
+
+class Animal{
+	void walk() {
+		System.out.println("걸을 수 있음");
+	}
+}
+
+class Bird extends Animal{
+	void fly() {
+		System.out.println("날 수 있음");
+	}
+	void sing() {
+		System.out.println("노래 부를 수 있음");
+	}
+}
+
+public class Chap6_Programming02 {
+
+	public static void main(String[] args) {
+		Bird bird = new Bird();  // Bird 객체 생성
+
+        // Animal 클래스의 메서드
+        bird.walk();  // 새도 걷는 행동 가능
+
+        // Bird 클래스의 고유 메서드
+        bird.fly();   // 새는 날 수 있음
+        bird.sing();  // 새는 노래할 수 있음
+
+	}
+
+}
