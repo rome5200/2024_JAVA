23c9aa71903dc5dd6706ab79c25af73c62ec6d09
rome5200 <144663246+rome5200@users.noreply.github.com>
2024-10-12 05:13:01 +0900

    Update pyramid.java

--- pyramid.java
+++ pyramid.java
@@ -16,14 +16,13 @@ public class pyramid {
 			System.out.println();
 		}
 
-		 // 반 피라미드 그리기
+    // 반 피라미드 그리기
     // 1. n줄 그린다
     // 2. 각 줄에서는 바깥 루프의 i개 만큼 그린다
-    for (int i = 1; i <= n; i++) {  // 몇번째 줄
-        // i개만큼 *을 그린다
-        for (int j = 1; j <= i; j++)
-            printf("*");
-        printf("\n");
+    for(int i = 1; i<=5;i++) {
+	for(int j = 1; j<=i; j++)
+		System.out.print("*");
+	System.out.println();
     }
 
     // 피라미드 그리기
@@ -31,14 +30,12 @@ public class pyramid {
     //      ***
     //     *****
     //    *******
-    for (int i = 1; i <= n; i++) {
-        // 빈칸: (n-i)개
-        for (int j = 1; j <= n - i; j++)
-            printf(" ");
-        // 별표: 2*i-1 개
-        for (int j = 1; j <= 2 * i - 1; j++)
-            printf("*");
-        printf("\n");
+    for(int i = 1; i<=5;i++) {
+	for(int j = 1; j<=5-i;j++)
+		System.out.print(" ");
+	for(int j = 1; j <= 2*i-1; j++)
+		System.out.print("*");
+	System.out.println();
     }
 
     // 반피라미드2
@@ -47,26 +44,26 @@ public class pyramid {
     // ..***
     // .****
     // *****
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n - i; j++)
-            printf(" ");
-        for (int j = 1; j <= i; j++)
-            printf("*");
-        printf("\n");
-    }
+    for(int i = 1; i<=5; i++) {
+	for (int j = 1; j <= 5-i; j++)
+		System.out.print(" ");
+	for (int j = 1; j <= i; j++)
+		System.out.print("*");
+	System.out.println();
+   }
 
     // 역피라미드
     // *******  i=1  빈칸 = i-1, * = 2*(n-i)+1
     // .*****
     // ..***
     // ...*
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= i - 1; j++)
-            printf(" ");
-        for (int j = 1; j <= 2 * (n - i) + 1; j++)
-            printf("*");
-        printf("\n");
-    }
+   for(int i = 1; i<= 5; i++) {
+	for(int j = 1; j<= i-1; j++)
+		System.out.print(" ");
+	for(int j = 1; j<=2*(5-i)+1;j++)
+		System.out.print("*");
+	System.out.println(" ");
 	}
+  }
 
 }
