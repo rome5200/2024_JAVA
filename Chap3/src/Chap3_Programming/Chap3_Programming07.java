0f94c015a2c4f7d136849bb296334e3ab5df2edd
rome5200 <144663246+rome5200@users.noreply.github.com>
2024-10-12 04:39:12 +0900

    Update Chap3_Programming07.java

--- Chap3/src/Chap3_Programming/Chap3_Programming07.java
+++ Chap3/src/Chap3_Programming/Chap3_Programming07.java
@@ -5,19 +5,13 @@ public class Chap3_Programming07 {
 	public static void main(String[] args) {
 		System.out.println("2부터 100 사이의 소수:");
 
-        for (int i = 2; i <= 100; i++) {
-            boolean isPrime = true;
-
-            for (int j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-
-            if (isPrime) {
-                System.out.print(i + " ");
-            }
-        }
+       for(int i=2; i<=100; i++) {
+		int j;
+		for(j=2; j<i; j++)
+			if(i%j == 0)
+				break;
+			if(j==i)
+				System.out.print(i+" ");
+		}
 	}
 }
