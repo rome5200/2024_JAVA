b37b2b6f810c6744b5e17f303baa98412a0b885e
rome5200 <144663246+rome5200@users.noreply.github.com>
2024-10-12 04:41:40 +0900

    Update Chap3_Programming07.java

--- Chap3/src/Chap3_Programming/Chap3_Programming07.java
+++ Chap3/src/Chap3_Programming/Chap3_Programming07.java
@@ -3,15 +3,13 @@ package Chap3_Programming;
 public class Chap3_Programming07 {
 
 	public static void main(String[] args) {
-		System.out.println("2부터 100 사이의 소수:");
-
-       for(int i=2; i<=100; i++) {
+		System.out.print("2부터 100 사이의 소수:");
+	    for(int i=2; i<=100; i++) {
 		int j;
 		for(j=2; j<i; j++)
 			if(i%j == 0)
 				break;
-			if(j==i)
-				System.out.print(i+" ");
+		if(j==i)
+			System.out.print(i+" ");
 		}
-	}
 }
