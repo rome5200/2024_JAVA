a3908b03bbb9d6b9530fda3d9368cf980b1d4fcc
rome5200 <144663246+rome5200@users.noreply.github.com>
2024-10-12 05:51:55 +0900

    Update pyramid.java

--- pyramid.java
+++ pyramid.java
@@ -65,5 +65,19 @@ public class pyramid {
 	System.out.println(" ");
 	}
   }
+  for(int i = 0; i< 5; i++) {
+	for(int j = 5-i; j>0 ; j--)
+		System.out.print("*");
+	System.out.println();
+  }
+
+	for(int i = 0; i< 5; i++) {
+		for(int j = 0; j < i; j++)
+			System.out.print(" ");
+		for(int j = 5-i; j>0 ; j--)
+			System.out.print("*");
+	System.out.println();
+	}
+	
 
 }
