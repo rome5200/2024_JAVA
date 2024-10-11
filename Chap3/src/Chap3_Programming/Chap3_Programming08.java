45a1b0d425e62ced310f6f5771ad31e4898bbc8d
rome5200 <144663246+rome5200@users.noreply.github.com>
2024-10-12 04:48:20 +0900

    Update Chap3_Programming08.java

--- Chap3/src/Chap3_Programming/Chap3_Programming08.java
+++ Chap3/src/Chap3_Programming/Chap3_Programming08.java
@@ -5,13 +5,17 @@ public class Chap3_Programming08 {
 	public static void main(String[] args) {
 		int cnt = 0;
 		
-		for (int i = 1; i < 100; i++) {
-			for (int j = 1; j < 100; j++)
-				for (int k = 1; k < 100; k++)
-					if((i*i+j*j)==(k*k)) {
-						System.out.printf("%d %d %d\n", i, j, k);
-						cnt++;
-					}
+		for(int a=1; a<=100; a++) {
+			for(int b=1; b<=100; b++)
+				for(int c=1; c<=100; c++) {
+					if(a*a+b*b==c*c)
+						System.out.println(a+"\t"+b+"\t"+c);
+					if(a*a+c*c==b*b)
+						System.out.println(a+"\t"+b+"\t"+c);
+					if(c*c+b*b==a*a)
+						System.out.println(a+"\t"+b+"\t"+c);
+				}
+		cnt++;
 		}
 		System.out.print(cnt+"개");
 	}
