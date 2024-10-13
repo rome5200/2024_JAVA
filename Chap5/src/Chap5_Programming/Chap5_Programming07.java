9431b2dd98af1bb73ffcebeb8abd4a5ab25e6bcd
rome5200 <144663246+rome5200@users.noreply.github.com>
2024-10-13 11:21:18 +0900

    Update Chap5_Programming07.java

--- Chap5/src/Chap5_Programming/Chap5_Programming07.java
+++ Chap5/src/Chap5_Programming/Chap5_Programming07.java
@@ -24,25 +24,23 @@ public class Chap5_Programming07 {
 		Scanner sc = new Scanner(System.in);
 		
 		while (true) {
-			System.out.print("검색할 영어 단어를 입력하세요(종료 : quit) : ");
+			System.out.print("input (종료 : quit) : ");
 			String input = sc.next();
-            if (input.equals("quit")) {
-                System.out.println("프로그램을 종료합니다.");
-                break;
-            }
-
-            boolean found = false;
-
-            for (Word w : list) {
-                if (w.eng.equals(input)) {
-                    System.out.println(w.eng+" -> "+w.kor);
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found) {
-                System.out.println("해당 단어는 사전에 없습니다.");
+			 if (input.equals("quit")) { 
+				 System.out.println("exit.");
+	             break;
+	         }
+			 boolean found = false;
+			 
+			 for (Word w : list) {
+	                if (w.eng.equals(input)) {
+	                    System.out.println(w.eng+" -> "+w.kor);
+	                    found = true;
+	                    break;
+	                }
+	         }
+			 if (!found) 
+				 System.out.println("not found");
             }
         }
 	}
